2b450ae1443a5d356b898038648adc1da3efd49a
yama-chiiii <nagayama0409@gmail.com>
2024-06-21 17:23:25 +0900

    ログインページのスマホサイズ

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,8 +1,7 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { auth } from './firebase-config';
-import logo from './image/logo.png'; // 画像ファイルをインポート
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase-config";
+import logo from "./image/logo.png"; // 画像ファイルをインポート
 
 const Login = () => {
     const navigate = useNavigate();
@@ -11,27 +10,24 @@ const Login = () => {
         const provider = new GoogleAuthProvider();
         signInWithPopup(auth, provider)
             .then((result) => {
-                console.log('ログイン成功:', result.user);
-                navigate('/home');
+                console.log("ログイン成功:", result.user);
+                navigate("/home");
             })
             .catch((error) => {
-                console.error('ログインエラー:', error);
+                console.error("ログインエラー:", error);
             });
     };
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-pink-100 font-sans">
-            <div className="flex flex-col items-center justify-center bg-white w-3/4 h-screen rounded-lg shadow-lg text-center">
-                <img src={logo} alt="Logo" className="mx-auto mb-24 w-96 h-96" />
-                <button
-                    onClick={signInWithGoogle}
-                    className="px-12 py-6 bg-pink-100 text-white text-xl rounded-lg hover:bg-pink-light"
-                >
+            <div className="flex flex-col items-center justify-center bg-white w-full sm:w-3/4 lg:w-1/2 h-screen rounded-lg shadow-lg text-center">
+                <img src={logo} alt="Logo" className="mx-auto mb-24 w-48 h-48 sm:w-72 sm:h-72 lg:w-96 lg:h-96" />
+                <button onClick={signInWithGoogle} className="px-12 py-6 bg-pink-100 text-white text-xl rounded-lg hover:bg-pink-light">
                     ろぐいん！
                 </button>
             </div>
         </div>
     );
-}
+};
 
 export default Login;
