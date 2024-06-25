da1540f95af933bb9ee6ec67a9f5ef3f1c9975cd
yama-chiiii <nagayama0409@gmail.com>
2024-06-26 03:23:18 +0900

    ローディング追加

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,16 +1,18 @@
 import { GoogleAuthProvider, getRedirectResult, signInWithRedirect } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase-config";
 import logo from "./image/logo.png"; // 画像ファイルをインポート
 
 const Login = () => {
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const userAgent = navigator.userAgent.toLowerCase();
 
     useEffect(() => {
         const fetchRedirectResult = async () => {
             console.log("Fetching redirect result...");
+            setLoading(true); // ローディングを開始
             try {
                 const result = await getRedirectResult(auth);
                 if (result && result.user) {
@@ -22,6 +24,8 @@ const Login = () => {
                 }
             } catch (error) {
                 console.error("ログインエラー:", error);
+            } finally {
+                setLoading(false); // ローディングを終了
             }
         };
 
@@ -35,9 +39,11 @@ const Login = () => {
 
     const signInWithGoogle = () => {
         console.log("Sign in with Google triggered");
+        setLoading(true); // ローディングを開始
         const provider = new GoogleAuthProvider();
         signInWithRedirect(auth, provider).catch((error) => {
             console.error("リダイレクト中のエラー:", error);
+            setLoading(false); // エラーが発生した場合、ローディングを終了
         });
     };
 
@@ -46,19 +52,25 @@ const Login = () => {
             <div className="flex flex-col items-center justify-center bg-white w-full sm:w-3/4 lg:w-1/2 h-screen rounded-lg shadow-lg text-center">
                 <img src={logo} alt="Logo" className="mx-auto mb-24 w-48 h-48 sm:w-72 sm:h-72 lg:w-96 lg:h-96" />
                 <p className="text-lg pb-8 text-pink-400">～おかし好きのためのかわいい記録帳♡～</p>
-                <button
-                    onClick={() => {
-                        console.log("Login button clicked");
-                        if (userAgent.includes("line")) {
-                            alert("LINE内ブラウザではGoogleログインがサポートされていません。外部ブラウザで開いてください。");
-                        } else {
-                            signInWithGoogle();
-                        }
-                    }}
-                    className="px-12 py-6 bg-pink-100 text-white text-xl rounded-lg hover:bg-pink-light"
-                >
-                    ろぐいん！
-                </button>
+                {loading ? (
+                    <div className="flex items-center justify-center">
+                        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-16 w-16"></div>
+                    </div>
+                ) : (
+                    <button
+                        onClick={() => {
+                            console.log("Login button clicked");
+                            if (userAgent.includes("line")) {
+                                alert("LINE内ブラウザではGoogleログインがサポートされていません。外部ブラウザで開いてください。");
+                            } else {
+                                signInWithGoogle();
+                            }
+                        }}
+                        className="px-12 py-6 bg-pink-100 text-white text-xl rounded-lg hover:bg-pink-light"
+                    >
+                        ろぐいん！
+                    </button>
+                )}
             </div>
         </div>
     );
