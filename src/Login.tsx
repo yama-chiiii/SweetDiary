2b6e33bdc3e54ade6d3e6dbd0a799186e32fa761
yama-chiiii <nagayama0409@gmail.com>
2024-06-26 01:19:47 +0900

    ログインページの修正

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,19 +1,10 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase-config";
 import logo from "./image/logo.png"; // 画像ファイルをインポート
 
 const Login = () => {
     const navigate = useNavigate();
-    const [isLineBrowser, setIsLineBrowser] = useState(false);
-
-    useEffect(() => {
-        const userAgent = navigator.userAgent.toLowerCase();
-        if (userAgent.includes("line")) {
-            setIsLineBrowser(true);
-        }
-    }, []);
 
     const signInWithGoogle = () => {
         const provider = new GoogleAuthProvider();
@@ -26,18 +17,6 @@ const Login = () => {
                 console.error("ログインエラー:", error);
             });
     };
-
-    if (isLineBrowser) {
-        return (
-            <div className="flex items-center justify-center min-h-screen bg-pink-100 font-sans">
-                <div className="flex flex-col items-center justify-center bg-white w-full sm:w-3/4 lg:w-1/2 h-screen rounded-lg shadow-lg text-center">
-                    <p className="text-lg pb-8 text-pink-400">
-                        LINEアプリ内のブラウザではログインできません。ChromeやSafariなどの標準ブラウザでアクセスしてください。
-                    </p>
-                </div>
-            </div>
-        );
-    }
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-pink-100 font-sans">
