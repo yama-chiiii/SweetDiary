99067a0535387d14c4458fe244f8d5ce1ecb2726
yama-chiiii <nagayama0409@gmail.com>
2024-06-26 02:02:09 +0900

    ログインページの修正

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,18 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase-config";
 import logo from "./image/logo.png"; // 画像ファイルをインポート
 
 const Login = () => {
     const navigate = useNavigate();
+    const userAgent = navigator.userAgent.toLowerCase();
+
+    useEffect(() => {
+        if (userAgent.includes("line")) {
+            alert("LINE内ブラウザではGoogleログインがサポートされていません。外部ブラウザで開いてください。");
+        }
+    }, [userAgent]);
 
     const signInWithGoogle = () => {
         const provider = new GoogleAuthProvider();
@@ -23,7 +31,16 @@ const Login = () => {
             <div className="flex flex-col items-center justify-center bg-white w-full sm:w-3/4 lg:w-1/2 h-screen rounded-lg shadow-lg text-center">
                 <img src={logo} alt="Logo" className="mx-auto mb-24 w-48 h-48 sm:w-72 sm:h-72 lg:w-96 lg:h-96" />
                 <p className="text-lg pb-8 text-pink-400">～おかし好きのためのかわいい記録帳♡～</p>
-                <button onClick={signInWithGoogle} className="px-12 py-6 bg-pink-100 text-white text-xl rounded-lg hover:bg-pink-light">
+                <button
+                    onClick={() => {
+                        if (userAgent.includes("line")) {
+                            alert("LINE内ブラウザではGoogleログインがサポートされていません。外部ブラウザで開いてください。");
+                        } else {
+                            signInWithGoogle();
+                        }
+                    }}
+                    className="px-12 py-6 bg-pink-100 text-white text-xl rounded-lg hover:bg-pink-light"
+                >
                     ろぐいん！
                 </button>
             </div>
