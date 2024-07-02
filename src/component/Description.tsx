f5b90ad0b1cb95c656fb79216e39c2a4f09e6aec
yama-chiiii <nagayama0409@gmail.com>
2024-07-02 19:32:46 +0900

    修正

--- src/component/Description.tsx
+++ src/component/Description.tsx
@@ -6,12 +6,23 @@ type DescriptionProps = {
 
 const Description: React.FC<DescriptionProps> = ({ onClose }) => (
   <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-    <div className="bg-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
+    <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-4 text-pink-500">
         おかしにっきとは？
       </h2>
       <p className="text-gray-700">
-        おかしにっきは、お菓子の価格やカロリー、味の感想を記録するための日記帳アプリです。日付の上にあるピンクのアイコンを押すと、その日に食べたお菓子の情報を入力できます。また、目標部分から月の目標も設定可能です。
+        ダイエット＆節約したい人向け！
+        <br />
+        毎日のお菓子の記録を付けるアプリ
+        <br />
+        【使い方】
+        <br />
+        ①ログインして、月の目標を設定する
+        <br />
+        ②記録したい日付をクリックして詳細を入力
+        <br />
+        ③目標を超えないようにしながらお菓子生活を楽しもう！
+        <br />
       </p>
       <button
         className="mt-4 px-4 py-2 bg-pink-100 text-white rounded-lg hover:bg-pink-200"
